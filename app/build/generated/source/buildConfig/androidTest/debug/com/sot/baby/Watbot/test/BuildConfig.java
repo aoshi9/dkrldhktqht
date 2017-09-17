2270389ed3a1d07b260c1dacd68272e8f16717a3
aoshi9 <koominkyoo@gmail.com>
2017-09-17 22:06:43 +0900

    카메라 에러 수정 및 conversation 정보 현행화(SOT)

--- app/build/generated/source/buildConfig/androidTest/debug/com/sot/baby/Watbot/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/com/sot/baby/Watbot/test/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.sot.baby.Watbot.test;
+package com.sot.baby.WatBot.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.sot.baby.Watbot.test";
+  public static final String APPLICATION_ID = "com.sot.baby.WatBot.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
